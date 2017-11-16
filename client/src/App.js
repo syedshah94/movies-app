d383c35e8e911f11789bdcc94064103561ad8427
lauren inacio <lauren_f23@yahoo.com>
2017-11-16 14:17:44 -0500

    fixed CONFLICTS in APP HEADER and FOOTER

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-const Header = () =>{
-  return(
-    <div>
+const Header = (props) => {
+  return (
     <header>
-    <div className='header'>
-      Header Component
-    </div>
-    <nav>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/login'>Login</Link></li>
-      <li><Link to='/register'>Register</Link></li>
-      <li><Link to='/dashboard'>Dashboard</Link></li>
-      <li><Link to='/movies'>Movies</Link></li>
-    </nav>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/movies">Movies</Link></li>
+          <li><span className="logout" onClick={props.logout}>Logout</span></li>
+        </ul>
+      </nav>
     </header>
-    </div>
-    )
-}
+  );
+};
 
-
-export default Header
+export default Header;
