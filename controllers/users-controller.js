d60af2fc269ef85e2d5b9eaa20581e687bf2aa06
lauren inacio <lauren_f23@yahoo.com>
2017-11-16 12:29:47 -0500

    lauren

---
+++ controllers/users-controller.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.js');
+
+const usersController = {};
+
+usersController.create = (req, res, next) => {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password_digest: hash,
+  }).then(user => {
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.status(201).json({
+        message: 'user successfully created',
+        auth: true,
+        data: {
+          user,
+        }
+      })
+    });
+  }).catch(next);
+}
+
+
+module.exports = usersController;
